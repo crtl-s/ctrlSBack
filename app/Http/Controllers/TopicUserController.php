89ee32236865ebae8e58978a4401da0bcf700118
juric70 <andjela.juric12@gmail.com>
2024-03-09 01:22:46 +0100

    Migrations, models and controllers created empty

---
+++ app/Http/Controllers/TopicUserController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TopicUserController extends Controller
+{
+    //
+}
---
+++ app/Models/KnowledgeLevel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KnowledgeLevel extends Model
+{
+    use HasFactory;
+}
