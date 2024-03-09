2a8d234386e45032f3957b12ed9d63c4063ec726
juric70 <andjela.juric12@gmail.com>
2024-03-09 03:04:41 +0100

    Basic crud finished

--- app/Http/Controllers/LessionUserController.php
+++ app/Http/Controllers/LessionUserController.php
@@ -2,9 +2,85 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LessionUser;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LessionUserController extends Controller
 {
     //
+    public function index(){
+        try{
+            $lession_users = LessionUser::with('lession')->with('user')->get();
+            return response()->json($lession_users, 200);
+
+        }catch (\Exception $e){
+            return response()->json($e->getMessage());
+
+        }
+    }
+    public function store(Request $request){
+        try
+        {
+            $validator = Validator::make($request->all(),[
+                'lession_id' => 'required|exists:lessions,id',
+                'user_id' => 'required|exists:users,id',
+            ]);
+            if ($validator->fails()){
+                return response()->json($validator->errors(), 400);
+            }
+            $lession_user = LessionUser::create([
+                'lession_id' => $request->lession_id,
+                'user_id' => $request->user_id,
+            ]);
+            return response()->json($lession_user, 201);
+        }catch (\Exception $e){
+            return response()->json($e->getMessage());
+
+        }
+    }
+    public function show($id){
+        try
+        {
+            $lession_user = LessionUser::findOrFail($id);
+            return response()->json($lession_user);
+        }catch (\Exception $e){
+            return response()->json($e->getMessage());
+
+        }
+    }
+
+    public function update(Request $request, $id){
+        try
+        {
+            $validator = Validator::make($request->all(),[
+                'lession_id' => 'required|exists:lessions,id',
+                'user_id' => 'required|exists:users,id',
+            ]);
+            if($validator->fails()){
+                return response()->json($validator->errors(), 400);
+            }
+            $lession_user = LessionUser::findOrFail($id);
+            $lession_user->update([
+                'lession_id' => $request->lession_id,
+                'user_id' => $request->user_id,
+            ]);
+
+            return response()->json($lession_user, 200);
+        }catch (\Exception $e){
+            return response()->json($e->getMessage());
+
+        }
+    }
+    public function destroy($id){
+        try
+        {
+            $lession_user = LessionUser::findOrFail($id);
+            $lession_user->delete();
+            return response()->json('Deleted successfully', 200);
+        }catch (\Exception $e){
+            return response()->json($e->getMessage());
+
+        }
+    }
 }
--- app/Http/Controllers/TopicUserController.php
+++ app/Http/Controllers/TopicUserController.php
@@ -2,9 +2,86 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TopicUser;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TopicUserController extends Controller
 {
     //
+    public function index(){
+        try{
+            $topic_users = TopicUser::with('topic')->with('user')->get();
+            return response()->json($topic_users, 200);
+
+        }catch (\Exception $e){
+            return response()->json($e->getMessage());
+
+        }
+    }
+    public function store(Request $request){
+        try
+        {
+            $validator = Validator::make($request->all(),[
+                'topic_id' => 'required|exists:topics,id',
+                'user_id' => 'required|exists:users,id',
+            ]);
+
+            if($validator->fails()){
+                return response()->json($validator->errors(), 400);
+            }
+            $topic_user = TopicUser::create([
+                'topic_id' => $request->topic_id,
+                'user_id' => $request->user_id,
+            ]);
+            return response()->json($topic_user, 201);
+        }catch (\Exception $e){
+            return response()->json($e->getMessage());
+
+        }
+    }
+    public function show($id){
+        try
+        {
+            $topic_user = TopicUser::findOrFail($id)->with('topic')->with('user')->get();
+            return response()->json($topic_user);
+        }catch (\Exception $e){
+            return response()->json($e->getMessage());
+
+        }
+    }
+
+    public function update(Request $request, $id){
+        try
+        {
+            $validator = Validator::make($request->all(),[
+                'topic_id' => 'required|exists:topics,id',
+                'user_id' => 'required|exists:users,id',
+            ]);
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 400);
+            }
+            $topic_user = TopicUser::findOrFail($id);
+            $topic_user->update([
+                'topic_id' => $request->topic_id,
+                'user_id' => $request->user_id,
+            ]);
+            return response()->json($topic_user, 200);
+        }catch (\Exception $e){
+            return response()->json($e->getMessage());
+
+        }
+    }
+
+    public function destroy($id){
+        try
+        {
+            $topic_user = TopicUser::findOrFail($id);
+            $topic_user->delete();
+            return response()->json(null, 204);
+        }catch (\Exception $e){
+            return response()->json($e->getMessage());
+
+        }
+    }
 }
